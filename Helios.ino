3c433dc65f1bd3dfd821260f4694ff202f9c4586
Jackson <jromie0924@gmail.com>
2015-03-28 17:39:40 -0400

    cleaning up unused code

--- Helios.ino
+++ Helios.ino
@@ -29,35 +29,12 @@ void setup() {
   Serial.begin(9600);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  //powerOn(strip.Color(255, 150, 110), 750);
-  powerOn(red, green, blue, 500);
+  powerOn(red, green, blue, 5);
   delay(1000);
-  powerOff();
+ // powerOff();
 }
 
 void loop() {
-  // Some example procedures showing how to display to the pixels:
- // colorWipe(strip.Color(255, 0, 0), 50); // Red
- // colorWipe(strip.Color(0, 255, 0), 50); // Green
- // colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  // Send a theater pxel chase in...
- // theaterChase(strip.Color(127, 127, 127), 50); // White
- // theaterChase(strip.Color(127,   0,   0), 50); // Red
- // theaterChase(strip.Color(  0,   0, 127), 50); // Blue
- // 255 152 96
- 
- //powerOn(0,100,0, 100);
- //delay(10);
- /*
- powerOn(strip.Color(255, 0, 0), 100);
- powerOn(strip.Color(0, 255, 0), 100);
- powerOn(strip.Color(0, 0, 255), 100);
- powerOn(strip.Color(255, 255, 255), 100);
-
-*/
- // rainbow(20);
- // rainbowCycle(20);
- // theaterChaseRainbow(50);
 }
 
 uint8_t splitColor ( uint32_t c, char value )
@@ -71,7 +48,6 @@ uint8_t splitColor ( uint32_t c, char value )
 }
 
 void powerOn(int r, int g, int b, int wait) {
- // uint8_t a = 0;
   for(int a = 0; a < strip.numPixels()/2; a++) {
     for(int i = 1; i <= 100; i++) {
       double percentage = (double)i/100;
@@ -85,7 +61,7 @@ void powerOn(int r, int g, int b, int wait) {
       strip.setPixelColor(a, strip.Color(red,green,blue));
       strip.setPixelColor(((strip.numPixels() - 1) - a), strip.Color(red,green,blue));
       strip.show();
-      delay(10);
+     // delay(1);
     }
     delay(wait);
   }
@@ -106,10 +82,10 @@ void powerOff() {
      // Serial.println(r);
       strip.setPixelColor(a, strip.Color((int)r,(int)g,(int)b));
       strip.setPixelColor(((strip.numPixels() - 1) -a), strip.Color((int)r,(int)g,(int)b));
-      delay(10);
+      //delay(10);
       strip.show();
     }
-    delay(10);
+    delay(1);
   }
 }
 
@@ -121,20 +97,6 @@ void colorWipe(uint32_t c, uint8_t wait) {
       delay(wait);
   }
 }
-
-/*
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-*/
 
 
 // Slightly different, this makes the rainbow equally distributed throughout
@@ -150,44 +112,6 @@ void rainbowCycle(uint8_t wait) {
   }
 }
 
-/*
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-*/
-/*
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-        }
-        strip.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, 0);        //turn every third pixel off
-        }
-    }
-  }
-}
-*/
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
