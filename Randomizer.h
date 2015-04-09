d675e3b4372baa22126c0cfd501685a52478de7e
Jackson Romie <jromie0924@gmail.com>
2015-04-08 22:41:49 -0400

    added Randomizer.h

---
+++ Randomizer.h
@@ -0,0 +1,14 @@
+#ifndef RANDOMIZER_H
+#define RANDOMIZER_H
+
+
+//using namespace std;
+
+class Randomizer {
+  public:
+    Randomizer();
+    void randomizePix(int, int); // for one pixel
+    void randomizePix(int, int, int); // for two pixels
+};
+
+#endif
