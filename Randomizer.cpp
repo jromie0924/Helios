5c9394a3e598d6e9c6091cd865c227731ffc751f
Jackson Romie <jromie0924@gmail.com>
2015-04-08 22:44:53 -0400

    added Randomizer.cpp

---
+++ Randomizer.cpp
@@ -0,0 +1,11 @@
+#include "Randomizer.h"
+
+Randomizer::Randomizer() {}
+
+void Randomizer::randomizePix(int rCol, int pix) {
+  
+}
+
+void Randomizer::randomizePix(int rCol, int pix1, int pix2) {
+  
+}
